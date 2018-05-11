ed6736436f13682f1563c8b0fb1e3059907179e5
Dmitry_Yashkin <day16@list.ru>
2018-05-11 18:00:55 +0300

    Class for frames

---
+++ src/MyJFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyJFrame extends JFrame {
+    public MyJFrame(String title, int width, int height){
+        super(title);
+        super.setSize(width, height);
+        super.setLocationRelativeTo(null);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        super.setLayout(new FlowLayout());
+        super.setVisible(true);
+    }
+
+    public MyJFrame(String title, int width, int height, Component component){
+        this(title, width, height);
+        super.getContentPane().add(component);
+    }
+}
