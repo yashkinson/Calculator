8e68b9dac71ef2ce69c030071d538abb5c5b5575
Dmitry_Yashkin <day16@list.ru>
2018-05-12 19:17:58 +0300

    Calculation of result

---
+++ src/CalcOperations.java
@@ -0,0 +1,16 @@
+public class CalcOperations {
+
+    public static double add(double a, double b){
+        return a + b;
+    }
+    public static double substract(double a, double b){
+        return a - b;
+    }
+    public static double multiply(double a, double b){
+        return a * b;
+    }
+    public static double divide(double a, double b){
+        return a / b;
+    }
+
+}
